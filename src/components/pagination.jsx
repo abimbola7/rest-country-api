535a8cf58138ac2ceac78c6dca6df875a8bddba6
abimbola7 <abimbolababajide6@gmail.com>
2023-10-06 17:02:32 +0100

    pagination component done

---
+++ src/components/pagination.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Pagination = ({ postPerpage, totalPost, paginate }) => {
+  const pageNumbers = [];
+  console
+  for(let i = 1; i <= Math.ceil(totalPost / postPerpage); i++) {
+    pageNumbers.push(i)
+  }
+  return (
+    <nav
+    className='py-10'
+    >
+      <ul className='flex flex-wrap justify-center'>
+        {
+          pageNumbers.map(number => (
+            <li className='dark:bg-darkBlue py-2 px-3 ml-2 mt-2 bg-white shadow-md' onClick={()=>paginate(number)} key={number}>{number}</li>
+          ))
+        }
+      </ul>
+    </nav>
+  )
+}
--- src/components/results.jsx
+++ src/components/results.jsx
@@ -2,16 +2,33 @@
 
 import React from 'react'
 import Card from './card'
+import { Pagination } from './pagination'
 import Searchbox from './searchbox'
 import Selectbox from './selectbox'
 
 const Results = ({ results }) => {
-  const [ filteredValue, setFilteredValue ] = React.useState()
+  const [ filteredValue, setFilteredValue ] = React.useState();
+  const [ currentPage, setCurrentPage ] = React.useState(1)
+  const [ postPerPage, setPostPerPage ] = React.useState(20)
+  const indexOfLastPost = currentPage * postPerPage
+  const indexOfFirstPost = indexOfLastPost - postPerPage
+  const currentPost =  filteredValue?.slice(indexOfFirstPost, indexOfLastPost);
+  const onPaginate = (value) => {
+    setCurrentPage(value);
+  };
   const filterHandler = (value) => {
-    console.log(filteredValue ? "works" : "not working")
-    const filter = results.filter(result => result.region === value)
-    setFilteredValue(filter)
+    if (value === "Choose a region") {
+      setFilteredValue(results)
+    } else{
+      const filter = results.filter(result => result.region === value)
+      setFilteredValue(filter)
+      setCurrentPage(1);
+    }
   }
+
+  React.useEffect(() => {
+    setFilteredValue(results)
+  }, [results])
   return (
     <div
     className="mx-4 lg:mx-auto max-w-7xl"
@@ -25,14 +42,14 @@ const Results = ({ results }) => {
       <div
       className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-16 gap-y-12 mt-20 place-items-center'
       >
+        
         {
-          !filteredValue ? results.map(result => (
-            <Card key={result.area} countries={result}/>
-          )) : filteredValue.map(result => (
+          currentPost?.map(result => (
             <Card key={result.area} countries={result}/>
           ))
         }
       </div>
+      <Pagination postPerpage={postPerPage} totalPost={filteredValue?.length} paginate={onPaginate}/>
     </div>
   )
 }
